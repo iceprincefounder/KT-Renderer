cf532b9b137d67a2169952f4f69c86c520d7b97d
iceprincefounder <iceprincefounder@qq.com>
2018-02-15 18:05:47 +0800

    create shader to return color

--- core.h
+++ core.h
@@ -21,22 +21,24 @@ namespace KT
 //
 
 class Object;
-class Material;
+class Shader;
 
 struct Intersection
 {
     Ray m_ray;
     float m_t;
     Object *m_pObject;
+    Shader *m_pShader;
     Color m_color;
-    Color m_emitted;
     Vector m_normal;
     
     
-    Intersection(): m_ray(), m_t(kRayTMax),
+    Intersection()
+        : m_ray(),
+          m_t(kRayTMax),
           m_pObject(NULL),
-          m_color(),
-          m_emitted(),
+          m_pShader(NULL),
+          m_color(1.0f, 1.0f, 1.0f),
           m_normal()
     {
         
@@ -46,8 +48,8 @@ struct Intersection
         : m_ray(i.m_ray),
           m_t(i.m_t),
           m_pObject(i.m_pObject),
+          m_pShader(i.m_pShader),
           m_color(i.m_color),
-          m_emitted(i.m_emitted),
           m_normal(i.m_normal)
     {
         
@@ -57,8 +59,8 @@ struct Intersection
          : m_ray(ray),
            m_t(ray.m_tMax),
            m_pObject(NULL),
-           m_color(),
-           m_emitted(),
+           m_pShader(NULL),
+           m_color(1.0f, 1.0f, 1.0f),
            m_normal()
     {
         
@@ -69,8 +71,8 @@ struct Intersection
         m_ray = i.m_ray;
         m_t = i.m_t;
         m_pObject = i.m_pObject;
+        m_pShader = i.m_pShader;
         m_color = i.m_color;
-        m_emitted = i.m_emitted;
         m_normal = i.m_normal;
         return *this;
     }
@@ -79,6 +81,71 @@ struct Intersection
     
     Point position() const { return m_ray.calculate(m_t); }
 };
+
+
+//
+// Shaders (scene hierarchy)
+//
+
+class Shader
+{
+public:
+    virtual ~Shader() { }
+    
+    // If the material is emitting, override this
+    virtual Color emittance() { return Color(); }
+    
+    virtual Color shade(const Point& position,
+                        const Vector& normal,
+                        const Vector& incomingRayDirection,
+                        const Vector& lightDirection) = 0;
+};
+
+// Lambertian diffuse material
+class Lambert : public Shader
+{
+public:
+    Lambert(const Color& color) : m_color(color) { }
+    
+    virtual ~Lambert() { }
+    
+    virtual Color shade(const Point& position,
+                        const Vector& normal,
+                        const Vector& incomingRayDirection,
+                        const Vector& lightDirectionNorm)
+    {
+        return std::max(0.0f, dot(lightDirectionNorm, normal)) * m_color;
+    }
+    
+protected:
+    Color m_color;
+};
+
+
+// Emitter (light) material
+class Emitter : public Shader
+{
+public:
+    Emitter(const Color& color, float power) : m_color(color), m_power(power) { }
+    
+    virtual ~Emitter() { }
+    
+    virtual Color emittance() { return m_color * m_power; }
+    
+    virtual Color shade(const Point& position,
+                        const Vector& normal,
+                        const Vector& incomingRayDirection,
+                        const Vector& lightDirectionNorm)
+    {
+        // Let the emittance take care of return color
+        return Color();
+    }
+    
+protected:
+    Color m_color;
+    float m_power;
+};
+
 
 
 //
@@ -248,7 +315,6 @@ public:
         intersection.m_t = t;
         intersection.m_pObject = this;
         intersection.m_color = Color();
-        intersection.m_emitted = m_color * m_power;
         intersection.m_normal = normal;
         // Hit the back side of the light?  We'll count it, so flip the normal
         // to effectively make a double-sided light.
@@ -266,7 +332,7 @@ public:
     {
         outNormal = cross(m_side1, m_side2).normalized();
         outPosition = m_position + m_side1 * u1 + m_side2 * u2;
-        // Reference point out in back of the light?  That's okay, we'll flip
+        // outPosition = m_position + m_side1  + m_side2 ;
         // the normal to have a double-sided light.
         if (dot(outNormal, outPosition - referencePosition) > 0.0f)
         {
@@ -279,6 +345,7 @@ protected:
     Point m_position;
     Vector m_side1, m_side2;
 };
+
 
 
 // Infinite-extent plane, with option bullseye texturing to make it interesting.
@@ -318,7 +385,6 @@ public:
         intersection.m_t = t;
         intersection.m_pObject = this;
         intersection.m_normal = m_normal;
-        intersection.m_emitted = Color();
         intersection.m_color = m_color;
         
         return true;
@@ -329,6 +395,148 @@ protected:
     Vector m_normal;
     Color m_color;
 };
+
+
+// Sphere (heh, what else?)
+class Sphere : public Object
+{
+public:
+    Sphere(const Point& position, float radius, Shader* pShader)
+        : m_position(position),
+          m_radius(radius),
+          m_pShader(pShader)
+    {
+        
+    }
+    
+    virtual ~Sphere() { }
+    
+    virtual bool intersect(Intersection& intersection)
+    {
+        Ray localRay = intersection.m_ray;
+        localRay.m_origin -= m_position;
+        
+        // Ray-sphere intersection can result in either zero, one or two points
+        // of intersection.  It turns into a quadratic equation, so we just find
+        // the solution using the quadratic formula.  Note that there is a
+        // slightly more stable form of it when computing it on a computer, and
+        // we use that method to keep everything accurate.
+        
+        // Calculate quadratic coeffs
+        float a = localRay.m_direction.length2();
+        float b = 2.0f * dot(localRay.m_direction, localRay.m_origin);
+        float c = localRay.m_origin.length2() - m_radius * m_radius;
+        
+        float t0, t1, discriminant;
+        discriminant = b * b - 4.0f * a * c;
+        if (discriminant < 0.0f)
+        {
+            // Discriminant less than zero?  No solution => no intersection.
+            return false;
+        }
+        discriminant = std::sqrt(discriminant);
+        
+        // Compute a more stable form of our param t (t0 = q/a, t1 = c/q)
+        // q = -0.5 * (b - sqrt(b * b - 4.0 * a * c)) if b < 0, or
+        // q = -0.5 * (b + sqrt(b * b - 4.0 * a * c)) if b >= 0
+        float q;
+        if (b < 0.0f)
+        {
+            q = -0.5f * (b - discriminant);
+        }
+        else
+        {
+            q = -0.5f * (b + discriminant);
+        }
+        
+        // Get our final parametric values
+        t0 = q / a;
+        if (q != 0.0f)
+        {
+            t1 = c / q;
+        }
+        else
+        {
+            t1 = intersection.m_t;
+        }
+        
+        // Swap them so they are ordered right
+        if (t0 > t1)
+        {
+            float temp = t1;
+            t1 = t0;
+            t0 = temp;
+        }
+        
+        // Check our intersection for validity against this ray's extents
+        if (t0 >= intersection.m_t || t1 < kRayTMin)
+        {
+            return false;
+        }
+        
+        if (t0 >= kRayTMin)
+        {
+            intersection.m_t = t0;
+        }
+        else if (t1 < intersection.m_t)
+        {
+            intersection.m_t = t1;
+        }
+        else
+        {
+            return false;
+        }
+        
+        // Create our intersection data
+        Point localPos = localRay.calculate(intersection.m_t);
+        Vector worldNorm = localPos.normalized();
+        
+        intersection.m_pObject = this;
+        intersection.m_pShader = m_pShader;
+        intersection.m_normal = worldNorm;
+        intersection.m_color = Color(1.0f, 1.0f, 1.0f);
+        
+        return true;
+    }
+    
+    // Given two random numbers between 0.0 and 1.0, find a location + surface
+    // normal on the surface of the *light*.
+    virtual bool sampleSurface(float u1, float u2, const Point& referencePosition, Point& outPosition, Vector& outNormal)
+    {
+        // Pick a random point on the whole sphere surface
+        outNormal = uniformToSphere(u1, u2);
+        outPosition = outNormal * m_radius + m_position;
+        if (dot(outNormal, referencePosition - outPosition) < 0.0f)
+        {
+            // Point was on the opposite side?  Flip around to make this
+            // more reasonable.
+            outNormal *= -1.0f;
+            outPosition = outNormal * m_radius + m_position;
+        }
+        return true;
+    }
+
+protected:
+    Point m_position;
+    float m_radius;
+    Shader *m_pShader;
+    
+    // Helper method for finding a random point on the sphere
+    Vector uniformToSphere(float u1, float u2)
+    {
+        // Find a height uniformly distributed on the sphere
+        float z = 1.0f - 2.0f * u1;
+        // Find the radius based on that height that sits on the sphere surface
+        float radius = std::sqrt(std::max(0.0f, 1.0f - z * z));
+        // Find a random angle around the sphere's equator
+        float phi = KT_PI * 2.0f * u2;
+        // And put it all together...
+        return Vector(radius * std::cos(phi),
+                      radius * std::sin(phi),
+                      z);
+    }
+};
+
 
 // Marsaglia multiply-with-carry psuedo random number generator.  It's very fast
 // and has good distribution properties.  Has a period of 2^60. See
